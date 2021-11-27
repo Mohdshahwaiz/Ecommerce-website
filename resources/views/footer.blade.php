490b55754969f865d8d9b5f83efbb2a9f65c74a3
Shahwaiz <MohdShahwaiz1122@gmail.com>
2021-11-27 15:49:27 +0530

    login added

---
+++ resources/views/footer.blade.php
@@ -0,0 +1,7 @@
+<h3>Footer will be here</h3>
+<div class="panel panel-default">
+    <div class="panel-body">
+      Panel content
+    </div>
+    <div class="panel-footer">Panel footer</div>
+  </div>
